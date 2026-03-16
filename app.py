49aa54e1be3595d2a68822f525c224d48276acad
Jayasri1409 <jayasriragavan5@gmail.com>
2026-03-16 22:19:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ products = [
 
 @app.route("/")
 def home():
-    return render_template("index.html", product=products)
+    return render_template("index.html", products=products)
 
 
 @app.route("/product/<int:id>")
