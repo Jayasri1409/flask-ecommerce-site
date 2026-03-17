d6fb49bf97e5234bfaa86e711d9055ce80b766c2
Jayasri1409 <jayasriragavan5@gmail.com>
2026-03-17 17:22:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-import os
-{from flask import Flask, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 products = [
-]
+
 {
 "id":1,
 "name":"SmartPhone",
@@ -68,12 +67,9 @@ def product(id):
 
     return render_template("product.html", product=selected_product)
 
+
 import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
